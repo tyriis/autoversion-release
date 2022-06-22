037d83385879e1c5a02c3019e6c88d9a425e1372
Nils Müller <nils.mueller@strg.at>
2022-06-23 00:10:38 +0200

    Change: print out pull request result

--- dist/index.js
+++ dist/index.js
@@ -8994,18 +8994,41 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(4249);
 const github = __nccwpck_require__(4705);
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run () {
+    try {
+        // `who-to-greet` input defined in action metadata file
+        const nameToGreet = core.getInput('who-to-greet');
+        console.log(`Hello ${nameToGreet}!`);
+        const time = (new Date()).toTimeString();
+        core.setOutput("time", time);
+        // Get the JSON webhook payload for the event that triggered the workflow
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        console.log(`The event payload: ${payload}`);
+        // This should be a token with access to your repository scoped in as a secret.
+        // The YML workflow will need to set myToken with the GitHub Secret Token
+        // myToken: ${{ secrets.GITHUB_TOKEN }}
+        // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+        const myToken = core.getInput('GITHUB_TOKEN');
+
+        const octokit = github.getOctokit(myToken)
+
+        // You can also pass in additional options as a second parameter to getOctokit
+        // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
+
+        const { data: pullRequest } = await octokit.rest.pulls.get({
+            owner: payload.base.repo.owner.login,
+            repo: payload.base.repo.name,
+            pull_number: payload.number,
+        });
+
+        console.log(`The pullRequest Payload: ${pullRequest}`);
+      } catch (error) {
+        core.setFailed(error.message);
+      }
 }
+
+run()
+
 })();
 
 module.exports = __webpack_exports__;
