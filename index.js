11bcb119bff7a755faf9a290f8555c800bccbc98
Nils Müller <nils.mueller@strg.at>
2022-06-23 13:04:04 +0200

    Change: rename to semantic release from label

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const RELEASE_TYPE_SKIP = 'skip'
+const RELEASE_TYPE_PATCH = 'patch'
+const RELEASE_TYPE_MINOR = 'minor'
+const RELEASE_TYPE_MAJOR = 'major'
+
+function getReleaseType (labels) {
+    if (labels.indexOf('skip-release') >= 0) {
+        return RELEASE_TYPE_SKIP
+    }
+    if (labels.indexOf('dev') >= 0 && labels.indexOf('dependencies') >= 0) {
+        return RELEASE_TYPE_PATCH
+    }
+    if (labels.indexOf('major') >= 0) {
+        return RELEASE_TYPE_MAJOR
+    }
+    if (labels.indexOf('minor')) {
+        return RELEASE_TYPE_MINOR
+    }
+    return RELEASE_TYPE_PATCH
+}
+
 async function run () {
     try {
-        // `who-to-greet` input defined in action metadata file
-        const nameToGreet = core.getInput('who-to-greet');
-        console.log(`Hello ${nameToGreet}!`);
-        const time = (new Date()).toTimeString();
-        core.setOutput("time", time);
         // Get the JSON webhook payload for the event that triggered the workflow
         const payload = JSON.stringify(github.context.payload, undefined, 2)
-        console.log(`The event payload: ${payload}`);
+        // console.log(`The event payload: ${payload}`)
         // This should be a token with access to your repository scoped in as a secret.
         // The YML workflow will need to set myToken with the GitHub Secret Token
         // myToken: ${{ secrets.GITHUB_TOKEN }}
         // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-        const myToken = core.getInput('GITHUB_TOKEN');
-
+        const myToken = core.getInput('GITHUB_TOKEN')
         const octokit = github.getOctokit(myToken)
-
-        // You can also pass in additional options as a second parameter to getOctokit
-        // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
-
         const { data: pullRequest } = await octokit.rest.pulls.get({
             ...github.context.repo,
-            //owner: github.context.payload.base.repo.owner.login,
-            //repo: github.context.payload.base.repo.name,
             pull_number: github.context.payload.number,
-        });
-
-        // await octokit.graphql()
-        console.log(`The pullRequest Payload: ${JSON.stringify(pullRequest, undefined, 2)}`);
-        console.log(`The pullRequest.labels Payload: ${JSON.stringify(pullRequest.labels, undefined, 2)}`);
+        })
+        // console.log(`The pullRequest Payload: ${JSON.stringify(pullRequest, undefined, 2)}`)
+        // console.log(`The pullRequest.labels Payload: ${JSON.stringify(pullRequest.labels, undefined, 2)}`)
+        const labels = pullRequest.labels.map((label) => label.name)
+        core.setOutput('releaseType', getReleaseType(labels))
       } catch (error) {
         core.setFailed(error.message);
       }
